91b31092d0070444f3808d2d7c5a8d073988044d
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-04-25 17:23:22 +0300

    init + added menu

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <main class="content">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface IMenuItem {
+  label: string
+  icon: string
+  path: string
+}
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Авторизация',
+    icon: 'pi pi-user',
+    path: '/auth',
+  },
+  {
+    label: 'Добавить',
+    icon: 'pi pi-plus',
+    path: '/',
+  },
+  {
+    label: 'Список собеседований',
+    icon: 'pi pi-list',
+    path: '/list',
+  },
+  {
+    label: 'Статистика',
+    icon: 'pi pi-chart-pie',
+    path: '/statistic',
+  },
+
+])
+</script>
+
+<template>
+  <AppMenuBar :model="items" class="menu">
+    <template #item="{item, props}">
+      <RouterLink :to="item.path" class="flex align-items-center" v-bind="props.action">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{item.label}}</span>
+      </RouterLink>
+    </template>
+  </AppMenuBar>
+</template>
+
+<style scoped>
+  .menu {
+    margin: 30px 0;
+  }
+
+  .menu-exit {
+    cursor: pointer;
+  }
+</style>
